622ba868f30c296bf866ded4b8d6aed7f3d74116
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-07-05 16:18:32 -0300

    test: adicionando testes da camada saleModel

--- tests/unit/models/salesModels.test.js
+++ tests/unit/models/salesModels.test.js
@@ -11,62 +11,46 @@ describe('models/saleModel', () => {
 
   describe('getAll', () => {
     it('deve retornar uma lista', () => {
-      sinon.stub(db, 'query').resolves([[{}]]);
-      chai.expect(saleModel.getAll()).to.eventually.be.true;
+      sinon.stub(db, 'query').resolves([[]]);
+      return chai.expect(saleModel.getAll()).to.eventually.be.deep.equal([]);
     });
 
     it('deve disparar um erro caso falhe o mysql', () => {
       sinon.stub(db, 'query').rejects();
-      chai.expect(saleModel.getAll()).to.eventually.be.rejected;
-    });
-    it('deve retornar undefined se não encontrar o produto', () => {
-      sinon.stub(db, 'query').resolves({});
-      chai.expect(saleModel.getAll()).to.eventually.be.false;
+      return chai.expect(saleModel.getAll()).to.eventually.be.rejected;
     });
   });
 
   describe('getById', () => {
     it('deve retornar um produto', () => {
-      sinon.stub(db, 'query').resolves({});
-      chai.expect(saleModel.getById(1)).to.eventually.be.true;
+      sinon.stub(db, 'query').resolves([{}]);
+      return chai.expect(saleModel.getById()).to.eventually.be.deep.equal({});
     });
     it('deve disparar um erro caso falhe o mysql', () => {
       sinon.stub(db, 'query').rejects();
-      chai.expect(saleModel.getById(1)).to.eventually.be.rejected;
-    });
-    it('deve retornar undefined se não encontrar o produto', () => {
-      sinon.stub(db, 'query').resolves(undefined);
-      chai.expect(saleModel.getById(1)).to.eventually.be.false;
+      return chai.expect(saleModel.getById(1)).to.eventually.be.rejected;
     });
   });
 
   describe('addSale', () => {
     it('deve adicionar um objeto', () => {
-      sinon.stub(db, 'query').resolves({});
-      chai.expect(saleModel.addSale()).to.eventually.be.true;
+      sinon.stub(db, 'query').resolves([{ insertId:1 }]);
+      return chai.expect(saleModel.addSale()).to.eventually.be.deep.equal(1);
     });
     it('deve disparar um erro caso falhe o mysql', () => {
       sinon.stub(db, 'query').rejects();
-      chai.expect(saleModel.addSale()).to.eventually.be.rejected;
-    });
-    it('deve retornar undefined se não encontrar o produto', () => {
-      sinon.stub(db, 'query').resolves(undefined);
-      chai.expect(saleModel.addSale()).to.eventually.be.false;
+      return chai.expect(saleModel.addSale()).to.eventually.be.rejected;
     });
   });
 
   describe('addSaleProduct', () => {
     it('deve retornar true', () => {
       sinon.stub(db, 'query').resolves(true);
-      chai.expect(saleModel.addSaleProduct(1, 1, 1)).to.eventually.be.true;
+      return chai.expect(saleModel.addSaleProduct(1, 1, 1)).to.eventually.be.true;
     });
     it('deve disparar um erro caso falhe o mysql', () => {
       sinon.stub(db, 'query').rejects();
-      chai.expect(saleModel.addSaleProduct(1, 1, 1)).to.eventually.be.rejected;
-    });
-    it('deve retornar undefined se não encontrar o produto', () => {
-      sinon.stub(db, 'query').resolves(undefined);
-      chai.expect(saleModel.addSaleProduct(1, 1, 1)).to.eventually.be.false;
+      return chai.expect(saleModel.addSaleProduct(1, 1, 1)).to.eventually.be.rejected;
     });
   });
 });
