f27f2b2fb414f1a0585eb24359b30e161887b997
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-07-06 19:49:32 -0300

    test: adicionando testes em Products controller

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -3,6 +3,7 @@ const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
 const { productsService } = require('../../../services/productsService');
 const { productsController } = require('../../../controllers/productsController');
+const { saleService } = require('../../../services/saleService');
 
 chai.use(chaiAsPromised);
 
@@ -11,45 +12,87 @@ const res = {};
 
 
 describe('controllers/productsController', () => {
-  beforeEach(sinon.restore);
+  let productMock;
 
   describe('getAll', () => {
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns();
+      productMock = sinon.stub(saleService, "listAll")
+    });
+
+    afterEach(sinon.restore);
+
     it('deve disparar um erro caso o listAll dispare um erro', () => {
-      sinon.stub(productsService, 'listAll').rejects();
+      productMock.rejects();
       return chai.expect(productsController.getAll(req, res)).to.be.eventually.rejected;
     });
   });
 
   describe('getById', () => {
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns();
+      productMock = sinon.stub(saleService, "getById")
+    });
+
+    afterEach(sinon.restore);
+
     it('deve disparar um erro caso o getById dispare um erro', () => {
-      sinon.stub(productsService, 'getById').rejects();
+      productMock.rejects();
       return chai.expect(productsController.getById(req, res)).to.be.eventually.rejected;
     });
   });
 
   describe('newProduct', () => {
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns();
+      productMock = sinon.stub(saleService, "validName")
+    });
+
+    afterEach(sinon.restore);
+
     it('deve disparar um erro caso o validName dispare um erro', () => {
-      sinon.stub(productsService, 'validName').rejects();
+      productMock.rejects();
       return chai.expect(productsController.newProduct(req, res)).to.be.eventually.rejected;
     });
   });
 
   describe('update', () => {
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns();
+      productMock = sinon.stub(saleService, "validName")
+      productMockExist = sinon.stub(saleService, 'exist')
+    });
+
+    afterEach(sinon.restore);
+
     it('deve disparar um erro caso o validName dispare um erro', () => {
-      sinon.stub(productsService, 'validName').rejects();
+      productMock.rejects();
       return chai.expect(productsController.update(req, res)).to.be.eventually.rejected;
     });
   });
 
-  it('deve disparar um erro caso o exist dispare um erro', () => {
-    sinon.stub(productsService, 'validName').resolves();
-    sinon.stub(productsService, 'exist').rejects();
-    return chai.expect(productsController.update(req, res)).to.be.eventually.rejected;
+    it('deve disparar um erro caso o exist dispare um erro', () => {
+      productMock.resolves();
+      productMockExist.rejects();
+      return chai.expect(productsController.update(req, res)).to.be.eventually.rejected;
   });
 
   describe('delete', () => {
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns();
+      productMockExist = sinon.stub(saleService, 'exist')
+    });
+
+    afterEach(sinon.restore);
+
     it('deve disparar um erro caso o exist dispare um erro', () => {
-      sinon.stub(productsService, 'exist').rejects();
+      productMockExist.rejects();
       return chai.expect(productsController.delete(req, res)).to.be.eventually.rejected;
     });
   });
